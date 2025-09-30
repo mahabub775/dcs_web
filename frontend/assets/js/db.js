3c6964cec31474cfc5a9ef52e8c816e859148b2d
mozahercse <Nipo@01839555609>
2025-09-30 20:30:03 +0600

    DB Ready..

--- frontend/assets/js/db.js
+++ frontend/assets/js/db.js
@@ -36,15 +36,18 @@ var menus=[
     }
 ];
 
-var contents=[
+var Home=[
     {
         id:1,
         name:'Home',
         title:'Welcome to <strong>Digital Communication System (DCS)</strong>',
         body:'<p>We are a licensed trading company based in Bangladesh. Established in the year of 2000 and connected with local and global markets through trusted international partnerships. It was built on the principles of sales, marketing, promotion of quality products and providing trustworthy service. It started business with number of reputed manufacturers and international principals. Its diversified product range continues to grow by following trends, improving standard products and listening to the customer. It participates in the tenders of government and public sectors. It is registered in <strong>Bangladesh Military, Para-military, Government organization and also Civil Organizations.</strong></p>'
-    },
+    }
+];
+
+var Aboutus=[
     {
-        id:2,
+        id:1,
         name:'Aboutus',
         title:'',
         body:'<p>Digital Communication System (DCS) offer goods and services at prices and conditions agreed by both the parties. Digital Communication System (DCS) is 25 (Plus) years of experience on indenting and supply, our team can provide accurate price of products as well as qualified producers, eliminating all the usual sourcing headaches and saving your business time and money. We are very active in the defense (Directorate General Defense Purchases) as well as other government and semi government organizations in Bangladesh. Digital Communication System (DCS) provides suitable technical service and customer assistance with respect to the goods in the territory on mutually agreed terms and conditions. Information about our progress, market situation, demands, sales, price trends for assortments, outlook for sales, latest customs regulations, packaging, advertising and other novelties. Clients are required to keep us informed about the quality of products they tend to place in the market by making samples, pamphlets, price lists, service instructions, reference lists, certificates etc available to us.<br>Digital Communication System (DCS) has a solid history in various lines of trading business. DCS has got a solid toehold in multifarious activities as a consequence of its dynamic supplying capabilities.<br><strong>We are officially enlisted with the Directorate General Defense Purchase (DGDP) to operate in both local and foreign currencies.</strong><br>In addition, we are authorized to serve as a commissioning agency and indenting partner for DGDP Acting as trusting local agent, we represent overseas principal sellers and manufacturers from the USA, Czech Republic, Germany, Slovenia, Poland, UK, turkey, south Korea, china, India, and many more.</p>'
@@ -112,8 +115,322 @@ var product=[
 var projects=[
     {
         id:1,
-        Title:'',
-        
-
+        Title:'Radio Communication',
+        description:`1. VHF/HF and UHF Manpack and Vehicular Military Radios and Spare Parts
+                    2. Hand-to-Hand Radio Set
+                    3. VHF/UHF Walkie-Talkie Radio Set (DMR)
+                    4. Repeater and Base Station`,
+    },
+    {
+        id:2,
+        Title:'Microwave',
+        description:`1. UHF Microwave Link with Spare Parts
+                    2. Microwave Self-Supporting Tower (Different Sizes)
+                    3. Microwave Guy Tower
+                    4. Earthing`,
+    },
+    {
+        id:3,
+        Title:'Communication',
+        description:``,
+    },
+    {
+        id:4,
+        Title:'Arms and Ammunition',
+        description:`1. Light Arms
+                        o 9mm Pistol
+                        o 12 Bore Shotgun
+                        o Sniper Rifle 7.62mm
+                        o Assault Rifle 7.62mm
+                        o SMG
+                    2. Heavy Arms
+                        o APC
+                        o 155mm Howitzer
+                        o Armored Military Vehicle
+                    3. Cartridge Arms
+                        o 9x19mm
+                        o 7.62mm
+                        o 45mm
+                        o 105mm
+                        o 125mm
+                        o 155mm`,
+    },
+    {
+        id:5,
+        Title:'Safety Equipment',
+        description:`• Bulletproof Helmet and Jacket
+                    • Explosive Detector
+                    • Mine Detector
+                    • Portable X-Ray Machine
+                    • Hook and Line Set
+                    • EOD Toolkit
+                    • CIED Robot
+                    • Bomb Jammer
+                    • EOD Suit`,
+    },
+    {
+        id:6,
+        Title:'Military Vehicles',
+        description:`• Fire Crash Tender
+                    • Air Crew Refueler
+                    • Forklift
+                    • Towing Tractor
+                    • GPU`,
+    },
+    {
+        id:7,
+        Title:'Aircraft',
+        description:`1. Aircraft (F-7, PT-6, K-8W, Yak-130, An-32 &amp; C-130) Engine and Airframe Spare Parts
+                    2. Avionics for Aircraft
+                    3. Engine and Aircraft Overhauling`,
+    },
+    {
+        id:8,
+        Title:'Helicopter',
+        description:`1. Helicopter Engine and Frame Spare Parts (MI-17/171, Bell-212, 206, 205, L-410, etc.)
+                    2. Helicopter Avionics
+                    3. Helicopter Engine and Overhauling`,
+    },
+    {
+        id:9,
+        Title:'Others',
+        description:`• Field Telephone Cables
+                    • Networking Equipment
+                    • Military Laptop
+                    • GPS
+                    • Night Vision Goggles
+                    • Night Vision Monocular
+                    • Aiming Circle 1mm
+                    • Aviation Compass
+                    • High-Protection Goggles
+                    • Microwave Trainer
+                    • Simulator (Mi-17, Firing, etc.)
+                    • Speedboat, etc.
+                    `,
     }
+]
+
+var Certificates=[
+    {
+        id:1,
+        Title:'A S M FOZLA E-LAHE TAX CERTIFICATE 2024-2025',
+        year:'2024-2025',
+        imgLink:``,
+    },
+    {
+        id:2,
+        Title:'Aviation Enlistment',
+        year:'2023-2024',
+        imgLink:``,
+    },
+    {
+        id:3,
+        Title:'BIN DCS',
+        year:'2023',
+        imgLink:``,
+    },
+    {
+        id:4,
+        Title:'BOF Certificate',
+        year:'2018',
+        imgLink:``,
+    },
+    {
+        id:5,
+        Title:'DCS Erc License[1]',
+        year:'2023-2024',
+        imgLink:``,
+    },
+    {
+        id:6,
+        Title:'DCS IRC',
+        year:'2024-2025',
+        imgLink:``,
+    },
+    {
+        id:7,
+        Title:'DCS Trade lice 2025 1',
+        year:'2022-2023',
+        imgLink:``,
+    },
+    {
+        id:8,
+        Title:'DCS Trade lice 2025 2',
+        year:'2025-2026',
+        imgLink:``,
+    },
+    {
+        id:9,
+        Title:'Indenting membership[1]',
+        year:'2023',
+        imgLink:``,
+    },
+    {
+        id:10,
+        Title:'Monogram',
+        year:'223-2024',
+        imgLink:``,
+    },
+    {
+        id:11,
+        Title:'NSSD Supplier ID',
+        year:'2024',
+        imgLink:``,
+    },
+    {
+        id:12,
+        Title:'TIN Fozle E-Lahi',
+        year:'2013',
+        imgLink:``,
+    },
+    {
+        id:13,
+        Title:'Trade Licance Re-New',
+        year:'2025-2026',
+        imgLink:``,
+    },
+    {
+        id:14,
+        Title:'Trade License',
+        year:'2022-2023',
+        imgLink:``,
+    },
+    {
+        id:15,
+        Title:'CAAB Certificate',
+        year:'2021',
+        imgLink:``,
+    },
+    {
+        id:16,
+        Title:'CAAB Thikadar',
+        year:'',
+        imgLink:``,
+    },
+    {
+        id:17,
+        Title:'CAAB Thikadar2',
+        year:'',
+        imgLink:``,
+    }
+]
+var Clients=[
+    {
+        id:1,
+        Title:'DGDP',
+        imgLink:``,
+    },
+    {
+        id:2,
+        Title:'NSSD',
+        imgLink:``,
+    },
+    {
+        id:3,
+        Title:'COD',
+        imgLink:``,
+    },
+    {
+        id:4,
+        Title:'BOF',
+        imgLink:``,
+    },
+    {
+        id:5,
+        Title:'Coast Guard',
+        imgLink:``,
+    },
+    {
+        id:6,
+        Title:'Bangladesh Police',
+        imgLink:``,
+    },
+    {
+        id:7,
+        Title:'BGB',
+        imgLink:``,
+    },
+    {
+        id:8,
+        Title:'Bangladesh Ansar',
+        imgLink:``,
+    },
+    {
+        id:9,
+        Title:'Fire Service &amp; Civil Defence',
+        imgLink:``,
+    },
+    {
+        id:10,
+        Title:'BIWTA',
+        imgLink:``,
+    },
+    {
+        id:11,
+        Title:'Civil Aviation',
+        imgLink:``,
+    },
+    {
+        id:12,
+        Title:'Bangladesh Aeronautical University',
+        imgLink:``,
+    },
+]
+
+var ContactUS=[
+    {
+        id:1,
+        Office:'Bangladesh office',
+        Title:'Digital communication system (Head Office)',
+        GISMap:``,
+        Address:'House no- 441 (4th floor), Road No- 30, Mohakhali DOHS, Dhaka- 1206, Bangladesh.',
+        Mobile:'+8801711443212',
+        Others:'Tel: +88-02-222293777  Fax: +88-02-224415098',
+        Email:'wcw1177@gmail.com',
+        Members:[
+            {
+                id:1,
+                Name:'A.S.M. Fozla elahe',
+                Designaion:'CEO',
+                Contact:'+881711443212',
+                Email:'',
+            },
+            {
+                id:2,
+                Name:'MD. Motiur Rahman Khan',
+                Designaion:'General Manager',
+                Contact:'',
+                Email:'',
+            },
+            {
+                id:3,
+                Name:'Jannatul Ferdous',
+                Designaion:'Accounts and Computer Analyzer',
+                Contact:'',
+                Email:'',
+            }
+        ]
+    },
+    {
+        id:2,
+        Office:'Hong Kong Office',
+        Title:'Four Seas Hong Kong Company (Global Office)',
+        GISMap:``,
+        Address:'unit 503,5/F, Silvercord Tower 2, 30 Canton Road, Tsimshatsui, Kowloon, Hong Kong',
+        Mobile:'',
+        Others:'Tel: 86-20-87374277  Fax: 86-20-87374270',
+        Email:'',
+        Members:[]
+    },
+    {
+        id:3,
+        Office:'TÜRKİYE office',
+        Title:'Auto Defense Co.Ltd (Global Office)',
+        GISMap:``,
+        Address:'Address: Söğütözü Cad. No:2 Koç Towers A Blok Daire 13 Çankaya/ANKARA - TÜRKİYE',
+        Mobile:'',
+        Others:'Tel: +90 312 945 48 97  Fax: 90 850 522 34 03',
+        Email:'',
+        Members:[]
+    },
 ]
